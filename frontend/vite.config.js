d05ebfc7e6167e8f1b9c10b64910f07bf9824138
celineahmim <celinea847@gmail.com>
2025-03-04 20:40:24 +0100

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Assurez-vous que le root est correct
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
+  server: {
+    port: 5173, // Assurez-vous que le port est libre
+  },
 });
